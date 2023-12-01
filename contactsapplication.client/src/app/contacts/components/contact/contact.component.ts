3e56e4fe9e6ad40ab07b95cd208aa36e121dea4c
Dominick <dom.galluzzo@gmail.com>
2023-12-01 11:04:33 -0500

    delete contact frontend

--- contactsapplication.client/src/app/contacts/components/contact/contact.component.ts
+++ contactsapplication.client/src/app/contacts/components/contact/contact.component.ts
@@ -1,6 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { EMPTY, catchError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { Contact } from '../../../core/models/contact.model';
+import { ContactsService } from '../../contacts.service';
+import { BaseError } from '../../../core/models/base-error.model';
+import { NotificationService } from '../../../core/services/notification-service.service';
 
 @Component({
   selector: 'app-contact',
@@ -9,4 +14,24 @@ import { Contact } from '../../../core/models/contact.model';
 })
 export class ContactComponent {
   @Input() contact: Contact;
+  @Output() deleted = new EventEmitter<number>();
+
+  constructor(
+    private contactsService: ContactsService,
+    private notificationService: NotificationService) {}
+
+  deleteContact(): void {
+    this.contactsService.deleteContact(this.contact.id).pipe(catchError((errorResponse: HttpErrorResponse) => {
+      const error = errorResponse.error as BaseError;
+      const message = !error.message ?
+        'An error was encountered while deleting contact' : error.message;
+
+      this.notificationService.createError(message, error.title);
+
+      return EMPTY;
+    }))
+    .subscribe((data: number) => {
+      this.deleted.emit(data);
+    })
+  }
 }
